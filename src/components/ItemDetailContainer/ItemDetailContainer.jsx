b3b5c52018458f569a539744f9a5d6378c71bc96
agustinafleitas <agustinaafd@gmail.com>
2024-08-06 00:23:45 -0300

    Firebase en ItemDetailContainer. Ajustes y funcionamiento de loading

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,23 +1,32 @@
 import { useEffect, useState } from 'react'
-import { getProductById } from '../../asyncMock'
 import './ItemDetailContainer.css'
 import { useParams } from 'react-router-dom'
 import { ItemDetail } from '../ItemDetail/ItemDetail'
+import { doc, getDoc } from 'firebase/firestore'
+import { DataBase } from '../../services/firebase'
 
 export const ItemDetailContainer = () => {
-  const [product, setProduct] = useState ()
-  const {id} = useParams()
+  const [product, setProduct] = useState (null);
+  const [loading, setLoading] = useState(true);
+  const { id } = useParams();
 
   useEffect(()=> {
-    getProductById(id)
-    .then((res)=>{
-      setProduct(res)
+    setLoading(true);
+    getDoc(doc(DataBase, "products", id))
+    .then((querySnapshot)=>{
+      const product = {id: querySnapshot.id, ...querySnapshot.data()}
+      setProduct(product)
     })
     .catch((err)=> console.log(err))
+    .finally(()=> setLoading(false));
   }, [id])
+
+  if (loading) {
+    return <h3>Cargando productos, por favor espere...</h3>
+  }
   return (
     <div>
-      <ItemDetail {...product}/>
+      {product ? <ItemDetail {...product}/> : <p>Producto no encontrado.</p>} 
     </div>
   )
 }
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -5,24 +5,38 @@ import { DataBase } from "../../services/firebase";
 import { collection, getDocs, query, where } from "firebase/firestore";
 
 const ItemListContainer = ({greetings}) => {
-    const [products, setProducts] = useState([])
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
     const { id } = useParams ();
 
     useEffect(()=>{
+        setLoading(true);
         const collectionRef = id
         ? query(collection(DataBase, "products"), where("category", "==", id))
         : collection(DataBase, "products")
 
         getDocs(collectionRef)
         .then((querySnapshot)=>{
+            console.log("querySnapshot:", querySnapshot);
             const products = querySnapshot.docs.map((doc)=>{
                 return {id: doc.id, ...doc.data()}
             })
             setProducts(products)
             console.log("Products fetched from Firebase:", products);
         })
-
+        .catch(error => {
+            console.log("Error fetching products:", error);
+        })
+        .finally(() => {
+            setLoading(false);
+        });
     }, [id]);
+
+    if (loading) {
+        return (
+            <h3>Cargando...</h3>
+        );
+    }
 
     return (
         <>
